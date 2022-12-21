63ad30a31e41be8810671bf74b4ed667b8c8b1a5
jmagundezg <jmagundezg@gmail.com>
2022-12-21 01:08:42 +0100

    removed singleton

--- src/config/config.py
+++ src/config/config.py
@@ -5,29 +5,11 @@ import json
 from typing import Optional
 
 
-class SingletonMeta(type):
-    "SingletonMeta type"
-    _instances = {}
 
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class ConfigSingleton(metaclass=SingletonMeta):
+class Config(object):
     """
     Class to save all the configuration needed in the project
     """
-
-    input_scheme: dict = {}
-    output_scheme: dict = {}
-
-    input_scheme_path: str = ""
-    output_scheme_path: str = ""
-
-    value_id_str: str = "$$"
 
     def __init__(
         self,
